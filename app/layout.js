da27db2ab2053a7eeaea6ab0b154d6ca176efb3e
Neha Kumari <nehakashyap1011@gmail.com>
2025-07-24 23:53:23 +0530

    created custom hook to fetch API

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "sonner";
 
 const inter = Inter({subsets : ['latin']});
 
@@ -20,6 +21,7 @@ export default function RootLayout({ children }) {
         {/* header */}
         <Header />
       <main className="min-h-screen">  {children}</main>
+      <Toaster richColors/>
         {/* footer */}
         <footer className="bg-[#c0ddfc8f] py-12">
 
